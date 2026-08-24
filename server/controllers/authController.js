23b2eef1b0eb0d45177a4b0abb2e65b73394071a
Tommax2 <martinsolumi@gmail.com>
2026-08-24 16:16:47 +0100

    addd

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,28 @@
 import User from '../models/User.js'
 export async function syncUser(req, res, next) {
   try {
-    const { uid, email, name, picture } = req.firebaseUser
-    const user = await User.findOneAndUpdate(
-      { firebaseUid: uid },
-      { $set: { email, name: name || email.split('@')[0], avatarUrl: picture || '' } },
-      { returnDocument: 'after', upsert: true, runValidators: true },
-    )
+    const { uid, email, name, picture, email_verified: emailVerified } = req.firebaseUser
+    const normalizedEmail = String(email || '').trim().toLowerCase()
+    const details = { email: normalizedEmail, name: name || normalizedEmail.split('@')[0], avatarUrl: picture || '' }
+    let user = await User.findOne({ firebaseUid: uid })
+
+    // Firebase may issue a new UID when an administrator deletes and the owner
+    // recreates a Google account. A verified email can safely reclaim its
+    // existing application data instead of creating a duplicate user.
+    if (!user) {
+      const existingEmail = await User.findOne({ email: normalizedEmail })
+      if (existingEmail && !emailVerified) throw Object.assign(new Error('Verify this email before restoring the existing account'), { statusCode: 409 })
+      if (existingEmail) {
+        existingEmail.firebaseUid = uid
+        Object.assign(existingEmail, details)
+        user = await existingEmail.save()
+      } else {
+        user = await User.create({ firebaseUid: uid, ...details })
+      }
+    } else {
+      Object.assign(user, details)
+      await user.save()
+    }
     res.json({ user: user.toPublicJSON() })
   } catch (error) { next(error) }
 }
